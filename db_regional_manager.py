5834c556bc863dc2fa81fa07d53aeb702e90a03a
Chad Phillips <chad@apartmentlines.com>
2020-08-02 16:26:30 -0700

    flesh out log levels, instance read/write

--- db_regional_manager.py
+++ db_regional_manager.py
@@ -16,29 +16,72 @@ DEFAULT_API_ENDPOINT = "clusters"
 
 class Manager(object):
 
-    def __init__(self, args, config):
-        self.args = args
-        self.config = config
+    def __init__(self, config_file=None, args={}):
+        self.config = self.parse_config(config_file or DEFAULT_CONFIG_FILE)
+        self.config.update(args)
+        self.quiet = 'quiet' in self.config
+        self.debug = 'debug' in self.config
         self.logger = logging.getLogger(self.__class__.__name__)
-        if self.args.quiet:
-            self.logger.setLevel(logging.ERROR)
-        if self.args.debug:
-            self.logger.setLevel(logging.DEBUG)
+        self.log_level = self.logger.level
+        if self.quiet:
+            self.enable_quiet()
+        if self.debug:
+            self.enable_debug()
 
-    def make_url(self):
-        return "http://%s:%s/api/%s" % (self.config['orchestrator']['host'], self.config['orchestrator']['port'], self.args.path)
+    def default_loglevel(self):
+        self.logger.setLevel(self.log_level)
 
-    def get(self):
-        self.logger.debug("Retrieving JSON data from : %s" % self.args.path)
-        pp.pprint(self.config)
+    def enable_debug(self):
+        self.logger.setLevel(logging.DEBUG)
+
+    def enable_quiet(self):
+        self.logger.setLevel(logging.ERROR)
+
+    def parse_config(self, config_file):
+        with open(config_file, 'r') as stream:
+            try:
+                config = yaml.safe_load(stream)
+            except yaml.YAMLError as err:
+                raise RuntimeError("Could not load config file %s: %s" % (config_file, err))
+            return config
+
+    def make_url(self, path=None):
+        path = path or self.config['path']
+        return "http://%s:%s/api/%s" % (self.config['orchestrator']['host'], self.config['orchestrator']['port'], path)
+
+    def parse_action_response(self, data):
+        success = data['Code'] == 'OK'
+        return success, data['Message']
+
+    def get(self, path):
+        self.logger.debug("Retrieving JSON data from : %s" % path)
         try:
-            response = requests.get(self.make_url(), auth=(self.config['orchestrator']['username'], self.config['orchestrator']['password']))
+            response = requests.get(self.make_url(path), auth=(self.config['orchestrator']['username'], self.config['orchestrator']['password']))
             response.raise_for_status()
             data = response.json()
-            pp.pprint(data)
+            if self.debug:
+                pp.pprint(data)
+            return data
         except Exception as err:
             self.logger.error('Error occurred: %s' % err)
             return False
+
+    def set_instance_read_only(self, hostname, port=3306):
+        self.logger.debug("Setting %s:%d writable" % (hostname, port))
+        return self.instance_action('set-read-only', hostname, port)
+
+    def set_instance_writeable(self, hostname, port=3306):
+        self.logger.debug("Setting %s:%d writable" % (hostname, port))
+        return self.instance_action('set-writeable', hostname, port)
+
+    def instance_action(self, path, hostname, port=3306):
+        data = self.get('%s/%s/%d' % (path, hostname, port))
+        if data:
+            success, message = self.parse_action_response(data)
+            self.logger.debug("Instance action message: %s" % message)
+            return success
+        return False
+
 
 def main():
     parser = argparse.ArgumentParser(description="Manage nftables sets")
@@ -48,14 +91,10 @@ def main():
     parser.add_argument("-p", "--path", type=str, metavar="PATH", default=DEFAULT_API_ENDPOINT, help="API endpoint, default: %s" % DEFAULT_API_ENDPOINT)
     #parser.add_argument("--berserk", action='store_true', help="Add all berserker_ips config to the resolver")
     #parser.add_argument("--sets", action='store', metavar="SET", type=str, nargs='+', help="Sets to update. Default is to update all sets in the configuration file")
-    args = parser.parse_args()
-    with open(args.config_file, 'r') as stream:
-        try:
-            config = yaml.safe_load(stream)
-        except yaml.YAMLError as err:
-            raise RuntimeError("Could not load config file %s: %s" % (args.config_file, err))
-        manager = Manager(args, config)
-        manager.get()
+    args = vars(parser.parse_args())
+    config_file = args.pop('config_file')
+    manager = Manager(config_file, args)
+    manager.get(args['path'])
 
 if __name__ == "__main__":
     main()
